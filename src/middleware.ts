edecf49f512c1cc20cdfbbc0248a1c5fa4d10172
Aayman-star <aayman.khalid1@gmail.com>
2024-07-23 10:28:59 +0500

    added NextRequest to middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware((auth, req: NextRequest) => {});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
