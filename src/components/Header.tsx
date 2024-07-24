546d079196c2e686788ba29e947d049058b5eb23
Aayman-star <aayman.khalid1@gmail.com>
2024-07-24 12:30:45 +0500

    made the header reponsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,19 @@ const Header = async () => {
           <UserButton />
         </SignedIn>
       </header>
-      <header className="md:hidden w-full py-8 px-4 bg-background border-b-[1px] border-muted-foreground"></header>
+      <header className="md:hidden w-full py-5 px-4 bg-background border-b-[1px] border-muted-foreground/50">
+        <SignedOut>
+          <div className="bg-muted px-2 py-1 w-20 grid place-content-center rounded ml-auto">
+            <SignInButton />
+          </div>
+        </SignedOut>
+        <SignedIn>
+          <div className="flex items-center justify-end">
+            <p className="px-2"> {capitalizeFirstLetter(username)}</p>
+            <UserButton />
+          </div>
+        </SignedIn>
+      </header>
     </>
   );
 };
