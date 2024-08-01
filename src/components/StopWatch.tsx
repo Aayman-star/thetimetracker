27c712cc200c0647e0450fcfb73ba5b5ac1c310f
Aayman-star <aayman.khalid1@gmail.com>
2024-08-01 13:37:19 +0500

    added an else in the stopwatch component

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -13,10 +13,15 @@ export const StopWatch = () => {
         <p className="py-2 text-primary">Waiting for stopwatch tasks</p>
       ) : (
         <div className="grid grid-col-1 lg:grid-cols-2 gap-4">
-          {stopWatchArray?.length > 0 &&
+          {stopWatchArray?.length > 0 ? (
             stopWatchArray.map((item, i) => (
               <StopWatchComponent key={item.id} {...item} />
-            ))}
+            ))
+          ) : (
+            <p className={`text-center py-2`}>
+              No tasks at the moment in the stopwatch
+            </p>
+          )}
         </div>
       )}
     </div>
